715c245a0ef5812c67f1cc9946a15b5da590ef95
Odos Matthews <odosmatthews@gmail.com>
2026-05-04 11:59:07 -0400

    chore(pyd3js-drag): bump to 0.1.0
    Update downstream dependency constraints to require pyd3js-drag>=0.1.0.

--- packages/pyd3js-drag/docs/CHANGELOG.md
+++ packages/pyd3js-drag/docs/CHANGELOG.md
@@ -4,3 +4,7 @@
 
 - (nothing yet)
 
+## 0.1.0
+
+- Initial port of `d3-drag@3.0.0`.
+
--- packages/pyd3js-drag/src/pyd3js_drag/__init__.py
+++ packages/pyd3js-drag/src/pyd3js_drag/__init__.py
@@ -6,6 +6,6 @@ from pyd3js_drag.drag import drag
 from pyd3js_drag.event import DragEvent
 from pyd3js_drag.nodrag import dragDisable, dragEnable
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 __all__ = ["__version__", "DragEvent", "drag", "dragDisable", "dragEnable"]
